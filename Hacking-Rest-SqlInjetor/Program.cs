a327e9f37a91bfe4755207102a73565a330795d7
rothexD <56934789+rothexD@users.noreply.github.com>
2020-11-13 22:45:53 +0100

    bwapp login

--- Hacking-Rest-SqlInjetor/Program.cs
+++ Hacking-Rest-SqlInjetor/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using Hacking_Rest_SqlInjetor.ServiceHandlers;
 using System.Threading.Tasks;
 using Hacking_Rest_SqlInjetor.ServiceHandlers.AttackHandlers;
+using Hacking_Rest_SqlInjetor.WebClient;
 
 namespace Hacking_Rest_SqlInjetor
 {
@@ -13,17 +14,33 @@ namespace Hacking_Rest_SqlInjetor
     class Program
     {
         static readonly string Data = "<form action=\"/sqli_1.php\" method=\"GET\"><p><label for=\"title\">Search for a movie:</label><input type=\"text\" value=\"hans\" id=\"title\" name=\"title\" size=\"25\"><input type=\"text\" id=\"title\" name=\"title\" size=\"25\"><input type=\"text\" id=\"title\" name=\"title\" size=\"25\"><input type=\"text\" id=\"title\" name=\"title\" size=\"25\"><input type=\"text\" id=\"title\" name=\"title\" size=\"25\"><input type=\"text\" id=\"title\" name=\"title\" size=\"25\"><input type=\"text\" id=\"title\" name=\"title\" size=\"25\"><button type=\"submit\" name=\"action\" value=\"search\">Search</button></p></form><form action=\"/abc.php\" method=\"POST\"><p><label for=\"title\">Search for a movie:</label><input type=\"text\" id=\"title\" name=\"keks\" size=\"25\"><button type=\"submit\" name=\"action\" value=\"search\">Search</button></p></form>";
-        private static readonly HttpClient _Client = new HttpClient();
 
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            string targetUri = "localhost:55001/login.php";
+            ICustomHttpClient _Client = new CustomHttpClient();
+           
 
-            AbstractServiceHandler Service = new JustParseNoAttack(_Client);
-            var doc = new HtmlAgilityPack.HtmlDocument();
-            doc.LoadHtml(Data);
-            var returnVal = Service.GetFormDataIntoDatabaseInformation(doc, targetUri);
+            const string loginPage = "http://localhost:55001/login.php";
+            var request = new HttpContext
+            {
+                RequestUri = new Uri(loginPage),
+                Method = HttpMethod.Post
+            };
+
+            request.AddField("login", "bee");
+            request.AddField("password", "bug");
+            request.AddField("security_level", "0");
+            request.AddField("form", "submit");
+            request.BuildRequest();
+
+            var response = _Client.Post(request).Result;
+            string responseString = response.Content.ReadAsStringAsync().Result;
+
+
+            AbstractServiceHandler Service = new XSS_attackGetMethod_Inputs();
+            Service.StartAttack("http://localhost:55001/xss_get.php", _Client);
+            Console.Read();
         }
     }
 }
