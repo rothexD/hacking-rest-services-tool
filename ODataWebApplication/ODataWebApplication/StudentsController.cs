c33a089a9fa0feee39b3bf8e30036e6794a65eae
Ngoc Lam Nguyen <if19b072@technikum-wien.at>
2020-11-19 18:19:14 +0100

    initial

---
+++ ODataWebApplication/ODataWebApplication/StudentsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ODataWebApplication
+{
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        [EnableQuery()]
+        public IEnumerable<Student> Get()
+        {
+            return new List<Student>
+            {
+                CreateNewStudent("Cody Allen", 130),
+                CreateNewStudent("Todd Ostermeier", 160),
+                CreateNewStudent("Viral Pandya", 140)
+            };
+        }
+
+        private static Student CreateNewStudent(string name, int score)
+        {
+            return new Student
+            {
+                Id = Guid.NewGuid(),
+                Name = name,
+                Score = score
+            };
+        }
+    }
+}
